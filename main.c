08d438ac43f3649ce7a95fa11a5479fb56d9454e
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-05-05 16:12:01 +0400

    Update main.c

--- main.c
+++ main.c
@@ -1,48 +1,48 @@
 #include "shell.h"
 
 /**
- *main - Main loop help us to write command and execute it.
- *@argc: number of arguments
- *@argv: list of arguments
- *Return: SUCCESS
- */
+* main - Main loop that tokenize input and execute commands
+* @argc: Number of arguments
+* @argv: List of arguments
+* Return: 0
+*/
+
 int main(int argc, char *argv[])
 {
-	char **args = NULL;
-	char *input = NULL;
-	int statusReturn = 1;
-	int exitStatus = 0;
-	size_t size = 0; /*Size represent size of buffer*/
-	ssize_t getLineReturn = 0;
-	/*(void)argc and (void)argv is used to handle unused parameter error.*/
-	(void) argc;
-	(void) argv;
-	/*Creating loop*/
-	while (statusReturn && getLineReturn != EOF)
+	char **arguments = NULL;
+	char *inputStdin = NULL;
+	int statusReturn = 1, exitStatus = 0;
+	size_t size = 0;
+	ssize_t returnValue = 0;
+    (void)argc;
+    (void)argv;
+
+    /*loop start*/
+    while (statusReturn && returnValue != EOF)
 	{
-		size = 0; /*set to 0 for every new input */
-		/*Return 1 if the file in the argument refers to the terminal */
+		size = 0;
+        /*isatty() - Check relation between terminal*/
 		statusReturn = isatty(STDIN_FILENO);
 		if (statusReturn)
 			write(STDOUT_FILENO, "$ ", 2);
-		getLineReturn = getline(&input, &size, stdin);/*Get input from user*/
-		if (getLineReturn == -1)/*-1 represent error*/
+		returnValue = getline(&inputStdin, &size, stdin);
+		if (returnValue == -1) /*error Case*/
 		{
-			free(input);
+			free(inputStdin);
 			break;
 		}
-		/*If only spaces, tabs and line breaks are writed*/
-		if (validateSpaces(input))
+		if (validateSpaces(inputStdin))
 		{
-			free(input);
+			free(inputStdin);
 			continue;
 		}
-		args = token(input); /*Divide input into tokens*/
-		if (*args[0] == '\0') /*If we get only spaces, tabs and line breaks */
+		arguments = token(inputStdin);
+		if (*arguments[0] == '\0')
 			continue;
-		statusReturn = executeBuiltins(args, input, argv, &exitStatus);
-		free(input);
-		free(args);
+		statusReturn = executeBuiltins(arguments, inputStdin,
+		argv, &exitStatus);
+		free(inputStdin);
+		free(arguments);
 	}
-	return (EXIT_SUCCESS);
+	return (0);
 }
