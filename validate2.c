926073ea54f03b01737794bd78b65e2c69159516
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-05-05 14:16:09 +0400

    Update validate2.c

--- validate2.c
+++ validate2.c
@@ -13,30 +13,44 @@ char *validateInput(char **args, char **argv)
 	int i = 0;
 
 	if (args[0][0] == '/' || args[0][0] == '.')
+	{
 		new_args = args[0];
+
 		if ((access(new_args, F_OK) == -1))
+		{
 			fprintf(stderr, "%s: %d: %s: No such file or directory\n",
 			argv[0], __LINE__, args[0]);
+
 			return ("Fail access");
+		}
+	}
 	else
+	{
 		strcpy(holder_env, getenv("PATH"));
-		tokens_path = tokenPath(holder_env);
+
+		tokens_path = tokenizer_path(holder_env);
 		while (tokens_path[i])
 		{
 			first = str_concat(slash_argument, args[0]);
 			new_args = str_concat(tokens_path[i], first);
 			if ((access(new_args, F_OK) == -1))
+			{
 				free(new_args);
 				free(first);
+			}
 			else
+			{
 				free(tokens_path);
 				free(first);
 				return (new_args);
+			}
 			i++;
 		}
 		free(tokens_path);
 		fprintf(stderr, "%s: %d: %s: No such file or directory\n",
-				argv[0], __LINE__, args[0]);
+		argv[0], __LINE__, args[0]);
+
 		return ("Fail access");
+	}
 	return (new_args);
 }
