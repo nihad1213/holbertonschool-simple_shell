caa9e6140046e5b9073ed43c8b0e10ede015e02e
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-05-05 12:48:12 +0400

    Update main.c

--- main.c
+++ main.c
@@ -1,42 +1,42 @@
 #include "shell.h"
 
 /**
- * main - Main loop help us to write command and execute it.
- * @argc: number of arguments
- * @argv: list of arguments
- * Return: SUCCESS  
+ *main - Main loop help us to write command and execute it.
+ *@argc: number of arguments
+ *@argv: list of arguments
+ *Return: SUCCESS  
  */
 int main(int argc, char *argv[])
 {
-    char **args = NULL;
-    char *input = NULL;
-    int statusReturn = 1;
-    int exitStatus = 0;
-    size_t size = 0; /*Size represent size of buffer*/
-    ssize_t getLineReturn = 0;
-    
-    /*(void)argc and (void)argv is used to handle unused parameter error.*/
-    (void)argc;
-    (void)argv;
+	char **args = NULL;
+	char *input = NULL;
+	int statusReturn = 1;
+	int exitStatus = 0;
+	size_t size = 0; /*Size represent size of buffer*/
+	ssize_t getLineReturn = 0;
 
-    /*Creating loop*/
-    while (statusReturn && getLineReturn != EOF)
-    {
-        size = 0; /* set to 0 for every new input */
+	/*(void)argc and (void)argv is used to handle unused parameter error.*/
+	(void) argc;
+	(void) argv;
+
+	/*Creating loop*/
+	while (statusReturn && getLineReturn != EOF)
+	{
+		size = 0; /*set to 0 for every new input */
 
 		/*Return 1 if the file in the argument refers to the terminal */
 		statusReturn = isatty(STDIN_FILENO);
 
 		if (statusReturn)
-            /*Write $ in buffer*/
+			/*Write $ in buffer*/
 			write(STDOUT_FILENO, "$ ", 2);
-        
-        /*Get input from user*/
+
+		/*Get input from user*/
 		getLineReturn = getline(&input, &size, stdin);
-		
-        /*-1 represent error*/
-        if (getLineReturn == -1)
-        {
+
+		/*-1 represent error*/
+		if (getLineReturn == -1)
+		{
 			free(input);
 			break;
 		}
@@ -45,15 +45,15 @@ int main(int argc, char *argv[])
 		if (validateSpaces(input))
 		{
 			free(input);
-            continue;
+			continue;
 		}
-		
-        /*Divide input into tokens*/
+
+		/*Divide input into tokens*/
 		args = token(input);
 		if (*args[0] == '\0') /*If we get only spaces, tabs and line breaks */
 			continue;
-		
-        statusReturn = executeBuiltins(args, input, argv, &exitStatus);
+
+		statusReturn = executeBuiltins(args, input, argv, &exitStatus);
 		free(input);
 		free(args);
 	}
