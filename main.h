73eb9e9255907d693a6ed5ec2cc6f1768ae90153
as1an87 <as1anoff87@gmail.com>
2024-05-05 18:13:08 +0000

    fix

--- main.h
+++ main.h
@@ -1,20 +1,24 @@
-#ifndef MAIN_H
-#define MATH_H
-
+#ifndef MAINH
+#define MAINH
+#define PATH "PATH="
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <fcntl.h>
-char **line_div(char *command,char **arr);
-char *_getline(void);
-int execute(char *command);
-void execute_child(char *command);
-int execute_parent(pid_t pid);
-void print_environment();
-void execute_absolute_path(char *arr[]);
-void execute_relative_path(char *arr[]);
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+extern char **path_var;
 extern char **environ;
-int main(void);
+char **get_command(char **);
+char **get_path(char **);
+char *find_file(char *command, int *);
+void print_env(void);
+void print_prompt(int status);
+void free_path(void);
+int exec_c(char **fcommand);
+char **_strtok(char *str, char delim);
+
 #endif
