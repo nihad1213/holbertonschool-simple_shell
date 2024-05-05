1ecf1cc1bd0f081b32ad75dc646c693aa139285c
as1an87 <as1anoff87@gmail.com>
2024-05-05 08:18:47 +0000

    Created .h file

---
+++ shell.h
@@ -0,0 +1,51 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/*SYSTEM HEADER FILES START*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <string.h>
+/*SYSTEM HEADER FLES END*/
+
+/*CONSTANTS START*/
+#define BUFFER_SIZE 64
+#define DELIMITERS " \t\n"
+/*CONSTANTS END*/
+
+/**
+ * struct chooseBuiltin - Builtin functions in in struct
+ * @nameBuiltin: Name of builtin functions
+ * @functionBuiltin: Pointer to builtin functions 
+ */
+typedef struct chooseBuiltin
+{
+    char *nameBuiltin;
+    int (*functionBuiltin)(char **, char *, int *);
+} chooseBuiltins;
+
+/*Glabol variable to access the environment list*/
+extern char **environ;
+
+/*FUNCTION PROTOTYPES START*/
+int cd(char **args, char *inputStdin, int *exitStatus);
+int setenvv(char **args,  char *inputStdin, int *exitStatus);
+int unsetenvv(char **args,  char *inputStdin, int *exitStatus);
+int envv(char **args, char *inputStdin, int *exitStatus);
+int exitt(char **args, char *inputStdin, int *exitStatus);
+int execute(char **args, char **argv, int *exitStatus);
+int executeBuiltins(
+    char **args, 
+    char *inputStdin,
+	char **argv, 
+    int *exitStatus
+    );
+char **token(char *input);
+char **tokenPath(char *input);
+char *validateInput(char **args, char **argv);
+int validateSpaces(char *input);
+/*FUNCTION PROTOTYPES END*/
+
+#endif
