f2f14acf91dc88aebfd80acedbc851e3e5d94c39
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-05-05 16:12:37 +0400

    Update validators.c

--- validators.c
+++ validators.c
@@ -2,72 +2,70 @@
 
 /**
 * validateInput - Validates if the PATH exists
-* @args: String of Tokens
-* @argv: String of arguments from input
+* @arguments: Arguments
+* @argv: Arguments from input
 * Return: A pointer to an array of the tokenized PATH directories
 */
-char *validateInput(char **args, char **argv) /* */
+char *validateInput(char **arguments, char **argv) /* */
 {
-	char *new_args, *first, *slash_argument = "/";
-	char **tokens_path, holder_env[1024];
+	char *newArguments, *first, *slashArgument = "/";
+	char **tokensPath, holderEnv[1024];
 	int i = 0;
 
 	/* check first element in array if starts with "." or*/
 	/*"/" character if so assumes arg is a path or executable file*/
-	if (args[0][0] == '/' || args[0][0] == '.')
+	if (arguments[0][0] == '/' || arguments[0][0] == '.')
 	{
 		/* point to the same address */
-		new_args = args[0];
+		newArguments = arguments[0];
 
 		/* check if file exists if not it points to error message */
-		if ((access(new_args, F_OK) == -1))
+		if ((access(newArguments, F_OK) == -1))
 		{
 			/* returns this message as error if file or path doesnt exist */
 			fprintf(stderr, "%s: %d: %s: No such file or directory\n",
-			argv[0], __LINE__, args[0]);
+			argv[0], __LINE__, arguments[0]);
 
 			return ("Fail access");
 		}
 	}
 	else
 	{
-		/* get PATH and copy it to holder_env buffer*/
-		strcpy(holder_env, getenv("PATH"));
+		/* get PATH and copy it to holderEnv buffer*/
+		strcpy(holderEnv, getenv("PATH"));
 
-		/* tokenize holer_env into array of directory PATH environment string */
-		tokens_path = token(holder_env);
-		while (tokens_path[i])
+		tokensPath = tokenPath(holderEnv);
+		while (tokensPath[i])
 		{
-			/* checkking if file exists if not frees allocated memory*/
-			/*for the full path and continues to next directory*/
-			first = str_concat(slash_argument, args[0]);
-			new_args = str_concat(tokens_path[i], first);
-			if ((access(new_args, F_OK) == -1))
+			first = str_concat(slashArgument, arguments[0]);
+			newArguments = str_concat(tokensPath[i], first);
+			if ((access(newArguments, F_OK) == -1))
 			{
-				free(new_args);
+				free(newArguments);
 				free(first);
 			}
 			else
 			{
-				free(tokens_path);
+				free(tokensPath);
 				free(first);
-				return (new_args);
+				return (newArguments);
 			}
 			i++;
 		}
-		free(tokens_path);
+
+		free(tokensPath);
 		fprintf(stderr, "%s: %d: %s: No such file or directory\n",
-		argv[0], __LINE__, args[0]);
+		argv[0], __LINE__, arguments[0]);
 
 		return ("Fail access");
 	}
-	return (new_args);
+	return (newArguments);
 }
 
 /**
 * validateSpaces - Checks if input is only spaces
 * @input: string from input
-* Return: 0 if there are spaces, tabs or line breaks
+* Return: 0
 */
 int validateSpaces(char *input)
 {
@@ -80,5 +78,6 @@ int validateSpaces(char *input)
 
 	if (i != len)
 		flag = 0;
+
 	return (flag);
 }
