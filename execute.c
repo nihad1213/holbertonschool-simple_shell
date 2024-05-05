b0cb1333a40024c07fe2553a1f29c8011c0c94de
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-05-05 16:10:54 +0400

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,85 +1,83 @@
 #include "shell.h"
 
 /**
- *execute - Fork process and replace the child
- *with a new program
- *@args: String of arguments (tokens)
- *@argv: Array of arguments
- *@exitStatus: Exit status
- *Return: 1
+ * execute - Fork process and replace the child
+ * with a new program
+ * @arguments: Arguments
+ * @argv: Array of arguments
+ * @exitStatus: Exit status
+ * Return: Always 1 (success)
  */
-int execute(char **args, char **argv, int *exitStatus)
+int execute(char **arguments, char **argv, int *exitStatus)
 {
 	pid_t pid;
-	/*It using for waitpid syscall*/
 	int status;
-	char *new_args;
+	char *newArguments;
 
-	new_args = validateInput(args, argv);
-	(void) exitStatus;
-	if (strcmp(new_args, "Fail access") == 0)
+	newArguments = validateInput(arguments, argv);
+	if (strcmp(newArguments, "Fail access") == 0)
 		return (1);
-	/*Fork create new process*/
 	pid = fork();
-	/*0 means child process*/
-	if (pid == 0)
+	if (pid == 0) /*Current child process*/
 	{
-		if (execve(new_args, args, environ) == -1)
+		if (execve(newArguments, arguments, environ) == -1)
 		{
 			perror("execve fail");
 			exit(EXIT_FAILURE);
 		}
 	}
-	/*<0 means error in fork*/
-	else if (pid < 0)
+	else if (pid < 0) /*fork fail*/
 	{
 		perror("Error forking");
-		free(new_args);
+		free(newArguments);
 		return (1);
 	}
 	else
 	{
 		waitpid(-1, &status, 0);
 		if (WIFEXITED(status))
-			*
-			exitStatus = WEXITSTATUS(status);
-		if (args[0][0] != '/' && args[0][0] != '.')
-			free(new_args);
+			*exitStatus = WEXITSTATUS(status);
+		if (arguments[0][0] != '/' && arguments[0][0] != '.')
+			free(newArguments);
 		return (1);
 	}
 	return (1);
 }
 
 /**
- *executeBuiltins - Choose from an array of builtin functions
- *@args: Arguments
- *@inputStdin: Input
- *@argv: List of arguments
- *@exitStatus: Exit status
- *Return: 1 or 0
+ * executeBuiltins - Choose from an array of builtin functions
+ *
+ * @arguments: Arguments (tokens)
+ * @inputStdin: String from input
+ * @argv: Array of arguments
+ * @exitStatus: Exit status
+ *
+ * Return: Status to stay or exit the main loop,
+ * 1 or 0
  */
-int executeBuiltins(char **args,
-	char *inputStdin,
-	char **argv,
-	int *exitStatus)
+int executeBuiltins(char **arguments, char *inputStdin,
+					char **argv, int *exitStatus)
 {
 	int i = 0;
-	chooseBuiltins options[] = {
-		{ "exit", exitt},
-		{ "env", envv},
-		{ "cd", cd},
-		{ "setenv", setenvv},
-		{ "unsetenv", unsetenvv},
+
+	choose_builtins_t options[] = {
+		{"exitt", exitt},
+		{"envv", envv},
+		{"cd", cd},
+		{"setenvv", setenvv},
+		{"unsetenvv", unsetenvv},
 		{NULL, NULL}
 	};
+
 	while (options[i].nameBuiltin)
 	{
-		if (strcmp(options[i].nameBuiltin, args[0]) == 0)
+		/* compare if arguments[0] matches the builtin function name */
+		if (strcmp(options[i].nameBuiltin, arguments[0]) == 0)
 		{
-			return ((int)((*options[i].functionBuiltin)(args, inputStdin, exitStatus)));
+			/* return the pointer to function (2nd memeber of array) and execute it */
+			return ((int)((*options[i].func_builtin)(arguments, inputStdin, exitStatus)));
 		}
-
 		i++;
 	}
-	return (execute(args, argv, exitStatus));
+	return (execute(arguments, argv, exitStatus));
 }
