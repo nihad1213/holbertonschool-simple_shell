f870ab609e99cf33addd858f67273c861803146d
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-05-05 16:59:22 +0400

    Update _getenv.c

--- _getenv.c
+++ _getenv.c
@@ -1,25 +1,49 @@
 #include "shell.h"
 /**
- * _getenv - gets the path
- * @path_name: a pointer to the struct of data
- *
- * Return: (Success) a positive number
+ * _getenv - gets an environment variable
+ * @name: environment variable to get
+ * Return: pointer to environment variable or NULL if there is no match
  */
-char *_getenv(const char *name) 
+char *_getenv(const char *name)
 {
-    extern char **environ;
-    char **env = environ;
+	char **environ_copy;
+	char *variable, *value, *path;
+	int compare;
+	unsigned int path_length, environ_length, length, i;
 
-    size_t namelen = strlen(name);
+	environ_length = 0;
+	while (environ[environ_length] != NULL)
+		environ_length++;
+	environ_copy = NULL;
+	environ_copy = copy_env(environ_copy, environ_length);
 
-    while (*env != NULL) 
-    {
-        if (strncmp(*env, name, namelen) == 0 && (*env)[namelen] == '=') 
+	length = _strlen((char *)name);
+	i = 0;
+	while (environ_copy[i] != NULL)
 	{
-            return *env + namelen + 1;
-        }
-        env++;
-    }
-
-    return NULL;
+		variable = environ_copy[i];
+		compare = _strncmp((char *)name, variable, length);
+		if (compare == 1)
+		{
+			value = strtok(variable, "=");
+			value = strtok(NULL, "\n ");
+			if (value == '\0')
+			{
+				errors(4);
+				exit(EXIT_FAILURE);
+			}
+			path_length = _strlen(value);
+			path = malloc(sizeof(char) * path_length + 1);
+			if (path == NULL)
+			{
+				errors(3);
+				return (NULL);
+			}
+			path = _strcpy(path, value);
+			free_dp(environ_copy, environ_length);
+			return (path);
+		}
+		i++;
+	}
+	return (NULL);
 }
