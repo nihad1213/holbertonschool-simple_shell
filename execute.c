38449825d4ee3e50d95715a1acf7ee634d8aaf77
Nihad <99197650+nihad1213@users.noreply.github.com>
2024-05-05 12:55:07 +0400

    Update execute.c

--- execute.c
+++ execute.c
@@ -14,8 +14,8 @@ int execute(char **args, char **argv, int *exitStatus)
 	/*It using for waitpid syscall*/
 	int status;
 	char *new_args;
+	
 	new_args = validateInput(args, argv);
-
 	(void) exitStatus;
 	if (strcmp(new_args, "Fail access") == 0)
 		return (1);
